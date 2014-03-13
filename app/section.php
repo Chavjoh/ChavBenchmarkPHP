8292c6e32ca8899b06f907653ca196e87495bf0e
Chavjoh <j.chavaillaz@gmail.com>
2014-03-13 10:44:13 +0100

    Save time performance in benchmark. Move benchmark to section class.

--- app/section.php
+++ app/section.php
@@ -30,12 +30,16 @@ class Section
 	protected $name;
 	protected $description;
 	protected $commandList;
+	protected $time; // Min, Max
+	
+	const KEY_MIN = 'min';
+	const KEY_MAX = 'max';
 	
 	public function __construct($name, $description)
 	{
 		$this->setName($name);
 		$this->setDescription($description);
-		
+		$this->time = null;
 		$this->commandList = array();
 	}
 	
@@ -67,5 +71,58 @@ class Section
 	public function getCommandList()
 	{
 		return $this->commandList;
+	}
+	
+	public function benchmark()
+	{
+		foreach ($this->getCommandList() as $command)
+		{
+			$time = $command->benchmark();
+			
+			if ($this->time == null)
+			{
+				$this->time = array(
+					Section::KEY_MIN => $time,
+					Section::KEY_MAX => $time
+				);
+			}
+			else
+			{
+				if ($time < $this->time[Section::KEY_MIN])
+					$this->time[Section::KEY_MIN] = $time;
+				else if ($time > $this->time[Section::KEY_MAX])
+					$this->time[Section::KEY_MAX] = $time;
+			}
+		}
+	}
+	
+	public function getFatestTime()
+	{
+		return $this->time[Section::KEY_MIN];
+	}
+	
+	public function getLongestTime()
+	{
+		return $this->time[Section::KEY_MAX];
+	}
+	
+	public function getDivisionFactor()
+	{
+		$time = $this->getLongestTime();
+		
+		$timeFactor = array(
+			1 => 's',
+			0.001 => 'ms',
+			0.0000001 => 'μs',
+			0.0000000001 => 'ns'
+		);
+		
+		foreach ($timeFactor AS $factor => $name)
+		{
+			if (($time / $factor) > 0)
+				return array($factor, $name);
+		}
+		
+		return array(null, null);
 	}
 }
