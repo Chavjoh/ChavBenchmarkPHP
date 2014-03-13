15c59081402b98fedfbc2211874cc09b85af4ef3
Chavjoh <j.chavaillaz@gmail.com>
2014-03-13 10:45:58 +0100

    Update template with new features.

--- app/template.php
+++ app/template.php
@@ -25,20 +25,62 @@
  * under the License.
  */
 ?>
-
-
-<?php foreach(Benchmark::getInstance()->getSectionList() AS $section): ?>
-	<h1> <?= $section->getName() ?> </h1>
-	<?= $section->getDescription() ?>
-	<table>
-		<?php foreach($section->getCommandList() AS $command): ?>
-		<tr>
-			<td><?= $command; ?></td>
-			<td><?= $command->getTime(); ?></td>
-			<td>Ratio</td>
-		</tr>
-		<?php endforeach; ?>
-	</table>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
 	
+	<title>PHP Benchmark</title>
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Main design -->
+	<link href="style/jumbotron-narrow.css" rel="stylesheet">
+	
+	<!-- Bootstrap -->
+	<link href="plugin/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-<?php endforeach; ?>
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<h3 class="text-muted">ChavBenchmarkPHP</h3>
+		</div>
+
+		<div class="jumbotron">
+			<h1>PHP Benchmark</h1>
+			<p class="lead">
+				...
+			</p>
+		</div>
+		
+		<?php foreach(Benchmark::getInstance()->getSectionList() AS $section): ?>
+			<?php
+			list($factor, $unity) = $section->getDivisionFactor();
+			?>
+			<h1> <?= $section->getName() ?> </h1>
+			<?= $section->getDescription() ?>
+			<table class="table table-striped">
+				<?php foreach($section->getCommandList() AS $command): ?>
+				<tr>
+					<td><?= round($command->getTime() / $factor, 5) ?> <?= $unity ?></td>
+					<td><?= round(($command->getTime() / $section->getFatestTime()) * 100, 1) ?>%</td>
+					<td><?= nl2br($command) ?></td>
+				</tr>
+				<?php endforeach; ?>
+			</table>
+		<?php endforeach; ?>
+
+		<div class="footer">
+			<p>By <a href="http://www.chavjoh.ch">Johan Chavaillaz</a>, under Apache 2.0 license.</p>
+		</div>
+
+	</div>
+</body>
+</html>
